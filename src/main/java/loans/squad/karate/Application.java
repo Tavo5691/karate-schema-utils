958c2a7d76b2cc11e778b25d10a671bc9bbf421f
Gustavo Bolivar <gustavo5691@hotmail.com>
2019-05-18 03:57:01 -0300

    Object parsing added

--- src/main/java/loans/squad/karate/Application.java
+++ src/main/java/loans/squad/karate/Application.java
@@ -1,29 +1,80 @@
 package loans.squad.karate;
 
 import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.google.common.base.CaseFormat;
+
 public class Application {
+	
+	private static List<String> schemaList = new ArrayList<>();
 
 	public static void main(String[] args) {
 //		Scanner scanner = new Scanner(System.in);
 //		System.out.println("Ingrese json a parsear");
-//		String inputJson = scanner.nextLine();
+//		String input = scanner.nextLine();
 //		scanner.close();
-		String inputJson = "{ \"meta\": { \"method\": \"PUT\", \"operation\": \"v1/pom/loans/holding/sarasa\", \"paging\": null, \"coso\": 1337655600000 } }";
+		//String input = "{ \"meta\": { \"method\": true, \"operation\": \"v1/pom/loans/holding/sarasa\", \"paging\": null, \"coso\": 1337655600000 } }";
+		String input = "{ \"meta\": { \"method\": \"PUT\", \"operation\": \"v1/pom/loans/holding/sarasa\", \"paging\": null }, \"data\": { \"loan_contract_account_identification\": { \"backend_id\": null, \"bank_country\": \"AR\", \"bank_key\": \"90000007\", \"account_number\": \"sarasa\" }, \"installment_number\": 0, \"due_date\": null }, \"errors\": [] }";
+		//String input = "{ \"meta\": { \"method\": \"PUT\", \"operation\": \"v1/pom/loans/holding/sarasa\", \"paging\": null }, \"data\": [ { \"loan_contract_account_identification\": { \"backend_id\": null, \"bank_country\": \"AR\", \"bank_key\": \"90000007\", \"account_number\": \"sarasa\" }, \"installment_number\": 0, \"due_date\": null } ], \"errors\": [] }";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("microserviceResponseSchema = { ");
 		
-		JSONObject input = new JSONObject(inputJson);
+		JSONObject inputJson = new JSONObject(input);
 		
-		JSONObject meta = input.getJSONObject("meta");
+		Set<String> inputKeys = inputJson.keySet();
+		Iterator<String> inputIterator = inputKeys.iterator();
 		
+		while (inputIterator.hasNext()) {
+			String inputKey = inputIterator.next();
+			String inputSchemaName = (CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, inputKey)) + "Schema";
+			
+			if (inputJson.get(inputKey) instanceof JSONObject) {
+				JSONObject currentObject = inputJson.getJSONObject(inputKey);
+				
+				String schema = getSchema(currentObject);
+				schemaList.add(inputSchemaName + " = " + schema);
+				sb.append(inputKey + ": '#(" + inputSchemaName + ")'");
+			} else if (inputJson.get(inputKey) instanceof JSONArray) {
+				JSONArray currentArray = inputJson.getJSONArray(inputKey);
+				
+				if (!currentArray.isEmpty()) {
+					JSONObject currentObject = currentArray.getJSONObject(0);
+					getSchema(currentObject);
+				} else {
+					sb.append(inputKey + ": []");
+				}
+			}
+			
+			
+			if (inputIterator.hasNext()) {
+				sb.append(", ");
+			}
+		}
+		
+		sb.append(" }");
+		String responseSchema = sb.toString();
+		
+		System.out.println(responseSchema);
+		for (String string : schemaList) {
+			System.out.println(string);
+		}
+
+	}
+	
+	private static String getSchema(JSONObject object) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("{ ");
 		
-		Set<String> keys = meta.keySet();
+		Set<String> keys = object.keySet();
 		Iterator<String> iterator = keys.iterator();
 		
 		while (iterator.hasNext()) {
@@ -31,14 +82,19 @@ public class Application {
 			sb.append(key);
 			sb.append(": ");
 			
-			Object value = meta.get(key);
+			Object value = object.get(key);
 			
 			if (value instanceof String) {
 				sb.append("'#string'");
 			} else if (value.equals(null)) {
 				sb.append("'#present'");
-			} else if (value instanceof Integer) {
+			} else if (value instanceof Integer || value instanceof Long) {
 				sb.append("'#number'");
+			} else if (value instanceof Boolean) {
+				sb.append("'#boolean'");
+			} else if (value instanceof JSONObject) {
+				String innerSchema = getSchema((JSONObject) value);
+				schemaList.add(key + "Schema = " + innerSchema);
 			}
 			
 			if (iterator.hasNext()) {
@@ -47,12 +103,6 @@ public class Application {
 		}
 		
 		sb.append(" }");
-		
-		System.out.println(sb.toString());
-
-	}
-	
-	private static void getSchema() {
-		
+		return sb.toString();
 	}
 }
