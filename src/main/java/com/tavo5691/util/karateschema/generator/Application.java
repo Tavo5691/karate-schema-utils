d96a29367a4df43b2317810f390d3f7822391c1f
Gustavo Bolivar <gustavo5691@hotmail.com>
2019-05-19 00:14:06 -0300

    Cleaned code, restructured packages, added javadoc

--- src/main/java/com/tavo5691/util/karateschema/generator/Application.java
+++ src/main/java/com/tavo5691/util/karateschema/generator/Application.java
@@ -1,11 +1,8 @@
-package loans.squad.karate;
+package com.tavo5691.util.karateschema.generator;
 
-import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -16,13 +13,19 @@ import com.google.common.base.CaseFormat;
 
 public class Application {
 	
+	// The list of schemas to be returned
 	private static List<String> schemaList = new ArrayList<>();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the service response JSON in one-line format:");
+		System.out.println("Enter the service's response JSON in one-line format:");
 		String input = scanner.nextLine();
 		scanner.close();
+		
+		/*
+		 * Test input JSONs in order of complexity. For testing uncomment one line and
+		 * comment out the lines above
+		 */
 		//String input = "{ \"meta\": { \"method\": true, \"operation\": \"v1/pom/loans/holding/sarasa\", \"paging\": null, \"coso\": 1337655600000 } }";
 		//String input = "{ \"meta\": { \"method\": \"PUT\", \"operation\": \"v1/pom/loans/holding/sarasa\", \"paging\": null }, \"data\": { \"loan_contract_account_identification\": { \"backend_id\": null, \"bank_country\": \"AR\", \"bank_key\": \"90000007\", \"account_number\": \"sarasa\" }, \"installment_number\": 0, \"due_date\": null }, \"errors\": [] }";
 		//String input = "{ \"data\": { \"amortization_system\": { \"amortization_system_code\": { \"value\": \"NN\", \"list_id\": null, \"list_agency_id\": null }, \"amortization_system_description\": \"FRANCES DIAS PERIODO =30,41666\" }}, \"errors\": [] }";
@@ -30,37 +33,34 @@ public class Application {
 		//String input = "{ \"meta\": { \"method\": \"PUT\", \"operation\": \"v1/pom/loans/holding/sarasa\", \"paging\": null }, \"data\": [ { \"loan_contract_account_identification\": { \"backend_id\": null, \"bank_country\": \"AR\", \"bank_key\": \"90000007\", \"account_number\": \"sarasa\" }, \"installment_number\": 0, \"due_date\": null } ], \"errors\": [] }";
 		//String input = "{ \"meta\": { \"method\": \"GET\", \"operation\": \"v1/pom/loans/I00124100212/details\", \"paging\": null }, \"data\": [ { \"operational_product\": null, \"bank_sales_product\": { \"bank_sales_product_id\": \"LHEHBC\", \"bank_sales_product_description\": \"LET HIPOTEC\", \"bank_sales_product_version\": null }, \"commitment_capital\": { \"value\": 370000, \"currency_code\": \"NN\", \"currency_code_list_id\": \"MONEDA\", \"currency_code_list_agency_id\": \"007\" }, \"dates\": { \"start_of_term\": 1337655600000, \"date_of_creation\": 1337655600000, \"end_of_term\": 1653274800000 }, \"sales_concept\": \"\", \"contract_manager\": { \"id\": \"241\" }, \"amortization_system\": { \"amortization_system_code\": { \"value\": \"NN\", \"list_id\": null, \"list_agency_id\": null }, \"amortization_system_description\": \"FRANCES DIAS PERIODO =30,41666\" }, \"use_of_founds\": { \"use_of_founds_code\": { \"value\": \"VIV\", \"list_id\": null, \"list_agency_id\": null }, \"use_of_founds_description\": \"COMPRA VIVIENDA UNICA\" }, \"debit_account\": { \"product\": { \"product_id\": null, \"currency\": { \"value\": \"01\", \"list_id\": \"MONEDA\", \"list_agency_id\": \"007\" }, \"nvproduct_id\": { \"subsystem_id\": 2, \"product_id\": \"CA\", \"sub_product_id\": \"GE\", \"modifier_id\": \"01\", \"sector_id\": \"T\" } }, \"account_identification\": { \"backend_id\": null, \"bank_country\": \"AR\", \"bank_key\": \"AR\", \"account_number\": \"241540107816\" } }, \"contract_status_code\": \"NN\", \"collateral_condition\": { \"collateral_condition_code\": { \"value\": \"LHI\", \"list_id\": \"GARANTIA\", \"list_agency_id\": \"310\" }, \"collateral_condition_description\": \"LET-HIP.S/MISMO INMUEBLE (P/VIV.PROPIA)\" }, \"interest_rate_type\": \"Vencida\", \"life_insurance\": { \"life_insurance_code\": { \"value\": \"NN\", \"list_id\": \"SEGURO VIDA\", \"list_agency_id\": \"310\" }, \"life_insurance_description\": \"VENCIDO COBRA DESDE EL 1ER VTO.\" }, \"goods_insurance\": { \"goods_insurance_code\": null, \"goods_insurance_description\": \"SEGURO DE GARANTIA (PREDEFINIDA)\" }, \"commission\": { \"commission_code\": { \"value\": \"NN\", \"list_id\": \"COMISION\", \"list_agency_id\": \"310\" }, \"commission_description\": \"ADELANTADA (COMIS.FIJA Y UNICA)\" }, \"credit_account\": { \"product\": { \"product_id\": null, \"currency\": { \"value\": \"01\", \"list_id\": \"MONEDA\", \"list_agency_id\": \"007\" }, \"nvproduct_id\": { \"subsystem_id\": null, \"product_id\": null, \"sub_product_id\": \"GE\", \"modifier_id\": null, \"sector_id\": \"T\" } }, \"account_identification\": { \"backend_id\": null, \"bank_country\": \"AR\", \"bank_key\": \"90000007\", \"account_number\": \"\" } }, \"amount_accredit\": { \"value\": 0, \"currency_code\": \"NN\", \"currency_code_list_id\": \"MONEDA\", \"currency_code_list_agency_id\": \"007\" }, \"number_of_installments\": { \"number_of_installments\": \"120 MESES\", \"total_number_of_installments\": 120, \"number_of_installments_outstanding\": 120 }, \"interest_rate_value\": 18, \"ref_interest_rate_value\": null, \"index_type\": null, \"calculate_type\": \"Simple\" } ], \"errors\": [] }";
 		
-		StringBuilder sb = new StringBuilder();
-		sb.append("microserviceResponseSchema = { ");
-		
 		JSONObject inputJson = new JSONObject(input);
-		System.out.println("Generating schemas...");
-		//Thread.sleep(250); 
+		StringBuilder sb = new StringBuilder();
+		
+		System.out.println("\nGenerating schemas...\n");
+		sb.append("microserviceResponseSchema = { ");
 		
 		Set<String> inputKeys = inputJson.keySet();
 		Iterator<String> inputIterator = inputKeys.iterator();
 		
 		while (inputIterator.hasNext()) {
 			String inputKey = inputIterator.next();
-			String inputSchemaName = getSchemaName(inputKey);
+			String inputSchemaName = formatSchemaName(inputKey);
 			
 			if (inputJson.get(inputKey) instanceof JSONObject) {
 				JSONObject currentObject = inputJson.getJSONObject(inputKey);
+				String schema = getSchema(currentObject);
 				
-				Map<String, String> schemaMap = getSchema(currentObject);
-				String schema = schemaMap.get("schema");
-				schemaList.add(0, inputSchemaName + " = " + schema);
 				sb.append(inputKey + ": '#(" + inputSchemaName + ")'");
+				schemaList.add(0, inputSchemaName + " = " + schema);
 			} else if (inputJson.get(inputKey) instanceof JSONArray) {
 				JSONArray currentArray = inputJson.getJSONArray(inputKey);
 				
 				if (!currentArray.isEmpty()) {
-					JSONObject currentObject = currentArray.getJSONObject(0);
+					JSONObject firstObject = currentArray.getJSONObject(0);
+					String schema = getSchema(firstObject);
 					
-					Map<String, String> schemaMap = getSchema(currentObject);
-					String schema = schemaMap.get("schema");
-					schemaList.add(0, inputSchemaName + " = " + schema);
 					sb.append(inputKey + ": '#[] (" + inputSchemaName + ")'");
+					schemaList.add(0, inputSchemaName + " = " + schema);
 				} else {
 					sb.append(inputKey + ": []");
 				}
@@ -80,8 +80,13 @@ public class Application {
 	}
 
 	
-	private static Map<String, String> getSchema(JSONObject object) {
-		Map<String, String> schemaMap = new HashMap<>();
+	/**
+	 * Gets the schema given a JSON object
+	 * 
+	 * @param object the JSON object to parse the schema from
+	 * @return the schema for the specified object with the references to other schemas if necessary
+	 */
+	private static String getSchema(JSONObject object) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("{ ");
 		
@@ -99,33 +104,31 @@ public class Application {
 				sb.append("'#string'");
 			} else if (value.equals(null)) {
 				sb.append("'#present'");
-			} else if (value instanceof Integer || value instanceof Long) {
+			} else if (value instanceof Integer || value instanceof Long || value instanceof Double) {
 				sb.append("'#number'");
 			} else if (value instanceof Boolean) {
 				sb.append("'#boolean'");
 			} else if (value instanceof JSONObject) {
-				String schemaName = getSchemaName(key);
+				String schemaName = formatSchemaName(key);
+				String innerObjectSchema = getSchema((JSONObject) value);
+				
 				sb.append("'#(" + schemaName + ")'");
-				schemaMap.put("schemaName", schemaName);
-				
-				Map<String, String> innerSchemaMap = getSchema((JSONObject) value);
-				String innerSchema = innerSchemaMap.get("schema");
-				schemaList.add(schemaName + " = " + innerSchema);
-				
+				schemaList.add(schemaName + " = " + innerObjectSchema);
 			} else if (value instanceof JSONArray) {
-				String schemaName = getSchemaName(key);
+				String schemaName = formatSchemaName(key);
 				JSONArray currentArray = (JSONArray) value;
 				
 				if (!currentArray.isEmpty()) {
-					JSONObject currentObject = currentArray.getJSONObject(0);
+					JSONObject firstObject = currentArray.getJSONObject(0);
+					String schema = getSchema(firstObject);
 					
-					Map<String, String> innerSchemaMap = getSchema(currentObject);
-					String schema = innerSchemaMap.get("schema");
-					schemaList.add(0, schemaName + " = " + schema);
 					sb.append("'#[] (" + schemaName + ")'");
+					schemaList.add(0, schemaName + " = " + schema);
 				} else {
 					sb.append("[]");
 				}
+			} else {
+				System.out.println("WARNING: Value not supported for key: " + key);
 			}
 			
 			if (iterator.hasNext()) {
@@ -134,17 +137,24 @@ public class Application {
 		}
 		
 		sb.append(" }");
-		String schema = sb.toString();
-		schemaMap.put("schema", schema);
-		return schemaMap;
+		return sb.toString();
 	}
 	
-	private static String getSchemaName(String inputKey) {
+	/**
+	 * Gets the correct schema name for the entered key
+	 * 
+	 * @param inputKey the key from a key-value pair in a JSON object in snake_case format
+	 * @return the schema name for such key in camelCase
+	 */
+	private static String formatSchemaName(String inputKey) {
 		return (CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, inputKey)) + "Schema";
 	}
 	
+	/**
+	 * Prints the schema list to the console
+	 */
 	private static void printOutput() {
-		System.out.println("Output:");
+		System.out.println("\nOutput:");
 		System.out.println("================================================================================");
 		
 		for (String string : schemaList) {
